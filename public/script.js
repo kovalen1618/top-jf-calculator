4a76c37783920506fb0baba1e08fbf264ed4d102
kovalen1618 <kovalen1.618@gmail.com>
2023-08-18 13:56:12 -0500

    Accessed digit DOM values and displayed them

--- public/script.js
+++ public/script.js
@@ -15,9 +15,21 @@ const divide = (numOne, numTwo) => {
 }
  
 
+// Populate display with chosen number and store for use in operate
+const display = document.getElementById('display');
 
+const digits = document.querySelectorAll('.digit');
+let displayValue = 0;
+
+digits.forEach(digit => digit.addEventListener('click', () => {
+    // Adds first digit together before operand and then next digit and then equals
+    display.innerText += digit.innerText;
+    displayValue += digit.innerText;
+}));
+
+
+
+// Equals
 const operate = (operation, numOne, numTwo) => {
     return operation(numOne, numTwo);
 }
-
-console.log(operate(multiply, 2, 3));
